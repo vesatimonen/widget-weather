4d5b98d6cc9710dd0f9f120357979c8e16852a63
Vesa Timonen <vtimonen@gmail.com>
2025-12-16 08:54:03 +0200

    Save point

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function parseOptions() {
 }
 
 
-var weatherData = undefined;
+let weatherData = undefined;
 async function getWeatherData() {
   try {
     const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=60.15031&longitude=24.88248&timezone=auto&temperature_unit=celsius&wind_speed_unit=ms&precipitation_unit=mm&forecast_days=2&current=is_day,uv_index,temperature_2m,precipitation,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m,apparent_temperature,relative_humidity_2m,cloud_cover,surface_pressure&hourly=is_day,uv_index,temperature_2m,precipitation_probability,relative_humidity_2m,apparent_temperature,precipitation,weather_code,surface_pressure,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=sunrise,sunset');
@@ -1240,27 +1240,27 @@ window.onload = function() {
     // Parse options
     parseOptions();
 
-    // Get weather data
-    getWeatherData();
+    // Get weather data (wait for results)
+    getWeatherData().then(() => {
+        // Fix canvas sizes
+        fixCanvasSize(document.querySelector(".wind-widget-canvas"));
+        fixCanvasSize(document.querySelector(".precipitation-canvas"));
+        fixCanvasSize(document.querySelector(".temperature-canvas"));
+        fixCanvasSize(document.querySelector(".wind-canvas"));
+        fixCanvasSize(document.querySelector(".uv-canvas"));
 
-    // Fix canvas sizes
-    fixCanvasSize(document.querySelector(".wind-widget-canvas"));
-    fixCanvasSize(document.querySelector(".precipitation-canvas"));
-    fixCanvasSize(document.querySelector(".temperature-canvas"));
-    fixCanvasSize(document.querySelector(".wind-canvas"));
-    fixCanvasSize(document.querySelector(".uv-canvas"));
+        // Draw current information into small widgets
+        drawCurrentWeather();
+        drawCurrentTemperature();
+        drawCurrentWind();
+        drawCurrentUVIndex();
 
-    // Draw current information into small widgets
-    drawCurrentWeather();
-    drawCurrentTemperature();
-    drawCurrentWind();
-    drawCurrentUVIndex();
+        // Draw forecast graphs
+        drawGraphs();
 
-    // Draw forecast graphs
-    drawGraphs();
-
-    // Show window
-    document.getElementById("screen").style.visibility = "visible";
+        // Show window
+        document.getElementById("screen").style.visibility = "visible";
+    });
 }
 
 
