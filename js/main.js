c72e2b3b8391154a8e5e3f3a927b99b582c5c55b
Vesa Timonen <vtimonen@gmail.com>
2025-12-16 09:29:27 +0200

    Save point

--- js/main.js
+++ js/main.js
@@ -31,9 +31,9 @@ async function getCurrentLocation() {
 * Get weather data
 *****************************************************************************/
 let weatherData = undefined;
-async function getWeatherData() {
+async function getWeatherData(latitude, longitude) {
   try {
-    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=60.15031&longitude=24.88248&timezone=auto&temperature_unit=celsius&wind_speed_unit=ms&precipitation_unit=mm&forecast_days=2&current=is_day,uv_index,temperature_2m,precipitation,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m,apparent_temperature,relative_humidity_2m,cloud_cover,surface_pressure&hourly=is_day,uv_index,temperature_2m,precipitation_probability,relative_humidity_2m,apparent_temperature,precipitation,weather_code,surface_pressure,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=sunrise,sunset');
+    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude +'&timezone=auto&temperature_unit=celsius&wind_speed_unit=ms&precipitation_unit=mm&forecast_days=2&current=is_day,uv_index,temperature_2m,precipitation,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m,apparent_temperature,relative_humidity_2m,cloud_cover,surface_pressure&hourly=is_day,uv_index,temperature_2m,precipitation_probability,relative_humidity_2m,apparent_temperature,precipitation,weather_code,surface_pressure,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=sunrise,sunset');
     weatherData = await response.json();
   } catch (err) {
     console.error(err);
