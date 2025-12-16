630693e34a52e3bcf6ffd0f43da72b4b99768635
Vesa Timonen <vtimonen@gmail.com>
2025-12-16 09:06:03 +0200

    Save point

--- js/main.js
+++ js/main.js
@@ -1,29 +1,6 @@
 const strorageName = "widget-weather";
 
-/*****************************************************************************
-* Parse URL options
-*****************************************************************************/
-function parseOptions() {
-    /* Get URL */
-    const url = new URL(window.location.href);
 
-    /* Level option */
-    const levelOption = url.searchParams.get("level");
-    if (levelOption == null) {
-    } else {
-    }
-}
-
-
-let weatherData = undefined;
-async function getWeatherData() {
-  try {
-    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=60.15031&longitude=24.88248&timezone=auto&temperature_unit=celsius&wind_speed_unit=ms&precipitation_unit=mm&forecast_days=2&current=is_day,uv_index,temperature_2m,precipitation,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m,apparent_temperature,relative_humidity_2m,cloud_cover,surface_pressure&hourly=is_day,uv_index,temperature_2m,precipitation_probability,relative_humidity_2m,apparent_temperature,precipitation,weather_code,surface_pressure,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=sunrise,sunset');
-    weatherData = await response.json();
-  } catch (err) {
-    console.error(err);
-  }
-}
 
 
 // Set canvas size
@@ -460,11 +437,56 @@ function drawGraphs() {
 }
 
 
+/*****************************************************************************
+* Get current location
+*****************************************************************************/
+function getCurrentLocation() {
+  return new Promise((resolve, reject) => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => resolve(position),
+        (error) => reject(error)
+      );
+    } else {
+      reject(new Error("Geolocation not supported"));
+    }
+  });
+}
+
+/*****************************************************************************
+* Parse URL options
+*****************************************************************************/
+function parseOptions() {
+    /* Get URL */
+    const url = new URL(window.location.href);
+
+    /* Level option */
+    const levelOption = url.searchParams.get("level");
+    if (levelOption == null) {
+    } else {
+    }
+}
+
+
+/*****************************************************************************
+* Get weather data
+*****************************************************************************/
+let weatherData = undefined;
+async function getWeatherData() {
+  try {
+    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=60.15031&longitude=24.88248&timezone=auto&temperature_unit=celsius&wind_speed_unit=ms&precipitation_unit=mm&forecast_days=2&current=is_day,uv_index,temperature_2m,precipitation,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m,apparent_temperature,relative_humidity_2m,cloud_cover,surface_pressure&hourly=is_day,uv_index,temperature_2m,precipitation_probability,relative_humidity_2m,apparent_temperature,precipitation,weather_code,surface_pressure,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=sunrise,sunset');
+    weatherData = await response.json();
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 // *** Main *******************************************************
 window.onload = function() {
     // Parse options
     parseOptions();
+
+
 
     // Get weather data (wait for results)
     getWeatherData().then(() => {
