ca6e58ab9851a44dd86f9e60888a7d4bb7b2b06f
Vesa Timonen <vtimonen@gmail.com>
2025-12-19 07:33:21 +0200

    Save point

--- js/main.js
+++ js/main.js
@@ -77,21 +77,19 @@ async function getWeatherData(latitude, longitude) {
 *****************************************************************************/
 async function getPlaceLocation(name) {
     try {
-/*
-        const query = "https://api.open-meteo.com/v1/forecast" +
-                      "?latitude=" + latitude + "&longitude=" + longitude +
-                      "&timezone=auto&temperature_unit=celsius&wind_speed_unit=ms&precipitation_unit=mm" +
-                      "&forecast_days=2" +
-                      "&current=is_day,uv_index,temperature_2m,precipitation,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m,apparent_temperature,relative_humidity_2m,cloud_cover,surface_pressure,precipitation_probability" +
-                      "&hourly=is_day,uv_index,temperature_2m,precipitation_probability,relative_humidity_2m,apparent_temperature,precipitation,weather_code,surface_pressure,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m" +
-                      "&daily=sunrise,sunset,sunshine_duration,daylight_duration,temperature_2m_min,temperature_2m_max,relative_humidity_2m_min,relative_humidity_2m_max,precipitation_sum,precipitation_hours,visibility_min,visibility_max";
+        const query = "https://geocoding-api.open-meteo.com/v1/search" +
+                      "?name=" + name +
+                      "&count=1" +
+                      "&language=en" +
+                      "&format=json";
 
         const response = await fetch(query);
-        weatherData = await response.json();
-*/
+        locationData = await response.json();
 
-        let location = {latitude: 60.1699, longitude: 24.9384};
-console.log(name, location.latitude, location.longitude);
+        let location = null;
+        if (locationData.results && locationData.results.length > 0) {
+            location = {latitude: locationData.results[0].latitude, longitude: locationData.results[0].longitude};
+        }
 
         return location;
     } catch (err) {
@@ -99,7 +97,7 @@ console.log(name, location.latitude, location.longitude);
         return null;
     }
 }
-// https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=1&language=en&format=json
+//
 
 
 /*****************************************************************************
@@ -160,6 +158,7 @@ window.onload = async function() {
 
         // Name as parameter?
         if (name != null) {
+            document.getElementById("header-title").innerHTML = "Get named location...";
             location = await getPlaceLocation(name);
         }
 
